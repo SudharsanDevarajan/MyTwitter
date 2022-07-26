33fd68e51a8f5b91b5352f4fdd5babed58f01f0c
sudharsan <sudharsan.devarajan@htcinc.com>
2022-07-26 10:42:55 +0530

    Initial Commit

---
+++ MyTwitter/MyTwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTwitterApp.swift
+//  MyTwitter
+//
+//  Created by htcuser on 26/07/22.
+//
+
+import SwiftUI
+
+@main
+struct MyTwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
